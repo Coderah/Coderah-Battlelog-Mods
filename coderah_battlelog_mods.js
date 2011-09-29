8c82dd7330ebe15784313a5caa1a3c53467751e8
Alex Howard <alex@howardsupply.com>
2011-09-29 01:54:56 -0400

    added code to skip password prompt; added update checking

--- coderah_battlelog_mods.js
+++ coderah_battlelog_mods.js
@@ -1,4 +1,22 @@
 launcher.verifyPassword = function(game, gameServerGuid, plaintextPassword, callback) { callback(true); }
+
+joinflow.showPasswordPromptPopup = function(server, callback)
+{
+	var pass = "fakepassword";
+
+	launcher.verifyPassword(server.game, server.guid, pass, function(passwordOk) {
+		if( passwordOk )
+		{
+			S.globalContext["session"]["serverPassword"] = pass;
+			callback();
+		}
+		else
+		{
+			base.showReceipt("Wrong password!", "skull");
+		}
+	} );
+}
+
 S.debug = function(msg)
 {
 	if (typeof msg == "object" && !(typeof JSON == "undefined")) {
@@ -7,3 +25,33 @@ S.debug = function(msg)
 		console.log("S.debug() - " + msg);
 	}
 } 
+
+base.showReceipt("Battlelog Mods - Password bypass ready.", receiptTypes.OK, 5000);
+
+//Extension auto update code
+function getExtensionVersion() {
+	return 1.3;
+}
+
+function createUpdateNotification() {
+	var updateReceipt = $('<div class="common-receipt type-checkbox" style="cursor: pointer;">' +
+		'<div class="common-receipt-message">' +
+		'Battlelog Mods -  update available, click here to update.' +
+		'</div>' +
+		'<div class="base-clear"></div>' +
+		'</div>');
+		
+	updateReceipt.click(function() {
+		base.showReceipt("Battlelog Mods - refresh to finalize update.", receiptTypes.OK, 5000);
+		$("body").append('<iframe src="http://coderah.com/bf3/battlelog_mods.crx"></iframe>');
+		$(this).remove();
+	});
+	$("#base-receipts").append(updateReceipt);
+}
+
+$.get("http://coderah.com/bf3/battlelog_mods_version.php", function(data) { 
+	console.log(data);
+	if (data.version > getExtensionVersion()) {
+		createUpdateNotification();
+	}
+});
