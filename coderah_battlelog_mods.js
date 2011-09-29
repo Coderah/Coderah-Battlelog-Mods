235d637011cb89c23a23f399161635a175961e93
Alex Howard <alex@howardsupply.com>
2011-09-29 00:21:21 -0400

    added json.stringify for debug objects

--- coderah_battlelog_mods.js
+++ coderah_battlelog_mods.js
@@ -1,5 +1,9 @@
 launcher.verifyPassword = function(game, gameServerGuid, plaintextPassword, callback) { callback(true); }
 S.debug = function(msg)
 {
-	console.log("S.debug() - " + msg);
+	if (typeof msg == "object" && !(typeof JSON == "undefined")) {
+		console.log("S.debug() object - " + JSON.stringify(msg));
+	} else {
+		console.log("S.debug() - " + msg);
+	}
 } 
