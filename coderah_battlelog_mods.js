eed176826602a45602d1ba9567cc1ae54f29f1f6
Alex Howard <alex@howardsupply.com>
2011-09-28 23:05:36 -0400

    first release.

---
+++ coderah_battlelog_mods.js
@@ -0,0 +1,5 @@
+launcher.verifyPassword = function(game, gameServerGuid, plaintextPassword, callback) { callback(true); }
+S.debug = function(msg)
+{
+	console.log("S.debug() - " + msg);
+} 
---
+++ loader.js
@@ -0,0 +1,5 @@
+var body = document.getElementsByTagName('body')[0];
+var script = document.createElement('script');
+script.type = 'text/javascript';
+script.src = chrome.extension.getURL( 'coderah_battlelog_mods.js');
+body.appendChild(script);
