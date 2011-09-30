6c48db70dc4a00a68f036fa374ecd6f348ce2cb9
Alex Howard <alex@howardsupply.com>
2011-09-30 07:35:03 -0400

    added built extensions (Chrome, GreaseMonkey)

---
+++ build/battlelog_mods.user.js
@@ -0,0 +1,256 @@
+// ==UserScript==
+// @name            Coderah Battlelog Mods
+// @author          Alex Howard
+// @namespace       http://www.coderah.com/?page_id=389
+// @description     Multiple modifications for Battlelog (bf3 beta)
+// @version	        1.6
+// @include         http://battlelog.battlefield.com/*
+// ==/UserScript==
+
+function coderah_battlelog_mods_main () {
+	var verboseDebug = false;
+
+var mods = {};
+
+var styleInject = document.createElement("style");
+styleInject.innerHTML = '#mod-menu { position:fixed; left:0px; top:0px; z-index:9999999;  background: #DFDFDF; width: 270px; height: auto; border: 1px solid #AAA;}#mod-menu .content-wrapper { padding-bottom:40px;}#mod-menu .content { padding:5px; font-family: Arial,sans-serif; color:#121212; font-size:14px;}#mod-menu .version { font-family: BebasNeueRegular,Arial,sans-serif; color: #343434; padding:3px; position:relative; top:10px; right:5px;}.mod-status { color: #3A3A3A; display:inline;}#mod-auto-join-button { font-size:12px !important;}#mod-menu .comcenter-settings { position:absolute; bottom:0px; width:100%; background: none repeat scroll 0 0 #F3F3F3; cursor: pointer;}#mod-menu-autojoin-status-box .text { font-family: BebasNeueRegular,Arial,sans-serif !important; font-weight: normal; text-decoration: none; font-size:16px;}#mod-menu-autojoin-status-box .mod-status { font-family: BebasNeueRegular,Arial,sans-serif; font-weight: normal; text-decoration: none; font-size:16px;}#mod-menu h1, #mod-menu h2, #mod-menu h3, #mod-menu h4 { font-family: BebasNeueRegular,Arial,sans-serif; font-weight: normal; text-decoration: none; color: #DBDBDB; display: block; text-align:center; background:rgba(0, 142, 163, 0.7); padding: 3px; /*padding-left:3px; color: #8A8A8A; height: 17px; line-height: 16px; background: #F4F4F4; border-top: 1px solid #F0F0F0; border-bottom: 1px solid #F0F0F0; font-size: 11px; font-weight: bold; text-transform: uppercase;*/}';
+document.body.appendChild(styleInject);
+
+$("body").append($('<div id="mod-menu"> <div class="comcenter-notification-title" id="mod-menu-header">BATTLELOG MOD - MENU  <div class="comcenter-contract">&nbsp;</div> </div>  <div class="content-wrapper">  <h3>STATUS</h3>  <div class="content">   Password Bypass: <div class="mod-status" id="mod-status-password-bypass">not ready</div><br>   AutoJoin Server: <div class="mod-status" id="mod-status-autojoin">not ready</div>  </div>    <span id="mod-menu-autojoin">  <h3>SERVER BROWSER</h3>  <div class="content">   <center>    <button class="base-button-arrow-large" id="mod-auto-join-button">AUTO JOIN SELECTED SERVER</button>   </center>   <div id="mod-menu-autojoin-status-box">    <span style="position:relative;top:7px;" class="text">State: <span class="mod-status"></span></span>    <button class="base-button-arrow-small-grey" style="float:right;" id="mod-auto-join-cancel">Cancel</button> <br clear="all">   </div>  </div>  </span>    <div class="comcenter-settings">   <div class="base-left">      </div>   <div class="base-right">   <span class="version"></span>   </div>   </div>  </div></div>'));
+
+$("#mod-menu .content-wrapper, #mod-menu-autojoin").hide();
+$("#mod-menu-header, #mod-menu .comcenter-settings").click(function() {
+	$("#mod-menu .content-wrapper").toggle();
+});
+
+mods.eventHandler = {
+	lastEvent: undefined,
+	callbacks: {},
+	
+	/* ## Events ##
+		launch_state - on launcher state change (error message usually in eventObject.launcherState.errorMessage)
+	*/
+	
+	addCallback: function(eventType, callback) {
+		
+		if (typeof this.callbacks[eventType] == "array") {
+			this.callbacks[eventType].push(callback);
+			mods.debug("added callback for eventType(" + eventType + ")");
+		} else {
+			this.callbacks[eventType] = [callback];
+			mods.debug("added callback for eventType(" + eventType + ")");
+		}
+	},
+	
+	runCallbacks: function(eventType, eventObject) {
+		for (var i in this.callbacks[eventType]) {
+			var callback = this.callbacks[eventType][i];
+			
+			callback(eventType, eventObject);
+		}
+	},
+	
+	eventTrigger: function(object) {
+		this.lastEvent = object;
+		
+		if (object.launcherState) {
+			this.runCallbacks("launch_state", object);
+		}
+	},
+}
+
+//password bypass
+mods.passwordBypass = {
+	state: "not ready",
+
+	apply: function() {
+		if (this.state == "not ready") {
+			launcher.verifyPassword = function(game, gameServerGuid, plaintextPassword, callback) { mods.debug("password bypassed"); callback(true); }
+
+			joinflow.showPasswordPromptPopup = function(server, callback)
+			{
+				var pass = "fakepassword";
+
+				launcher.verifyPassword(server.game, server.guid, pass, function(passwordOk) {
+					if( passwordOk )
+					{
+						S.globalContext["session"]["serverPassword"] = pass;
+						callback();
+					}
+					else
+					{
+						base.showReceipt("Wrong password!", "skull");
+					}
+				} );
+			}
+			
+			this.state = "ready";
+			this.setMenuState();
+			base.showReceipt("Battlelog Mods - Password bypass ready.", receiptTypes.OK, 5000);
+			mods.debug("passwordBypass applied");
+		}
+	},
+	
+	setMenuState: function() {
+		$("#mod-status-password-bypass").html(this.state);
+	}
+};
+
+//autojoin
+mods.autoJoin = {
+	modState: "not ready",
+	state: 0,
+	server: null,
+	joinTimeout: null,
+	
+	errorMessagesForRetry: ["Could not join server since it's full", "Can't join, this server is changing map, please try again soon.", "Could not join server since it couldn't be found."],
+	
+	join: function() {
+		mods.autoJoin.setStatus("trying...");
+		joinflow.joinServerByUrl(mods.autoJoin.server, null, function(serverFound) {
+			if (serverFound) {
+				mods.debug("server found by url");
+			}
+		});
+	},
+	
+	showStatusBox: function() {
+		$("#mod-auto-join-button").hide();
+		$("#mod-menu-autojoin-status-box").show();
+		this.setStatus("waiting...");
+	},
+	
+	hideStatusBox: function() {
+		$("#mod-menu-autojoin-status-box").hide();
+		$("#mod-auto-join-button").show();
+	},
+	
+	setStatus: function(text) {
+		$("#mod-menu-autojoin-status-box .mod-status").html(text);
+	},
+	
+	cancel: function() {
+		this.state = 0;
+		clearTimeout(this.joinTimeout);
+		mods.autoJoin.hideStatusBox();
+	},
+	
+	apply: function() {
+		if (this.modState == "not ready") {
+			mods.eventHandler.addCallback("launch_state", function(eventType, eventObject) {
+				//mods.debug("launch_state event = "+JSON.stringify(eventObject.launcherState));
+				if (mods.autoJoin.state == 1) {
+					if (eventObject.launcherState.name == "launch_error" && mods.autoJoin.errorMessagesForRetry.indexOf(eventObject.launcherState.errorMessage) > -1) {
+						mods.autoJoin.joinTimeout = setTimeout(mods.autoJoin.join, 1500);
+						mods.debug("autojoin attempting connection again (" + eventObject.launcherState.errorMessage + ")");
+						mods.autoJoin.setStatus("waiting...");
+					}
+					if ((eventObject.launcherState.gameState && eventObject.launcherState.gameState == "State_ConnectToGameId") || (eventObject.launcherState.name == "launch_cancelling")) {
+						mods.autoJoin.cancel();
+						mods.debug("autojoin finished");
+					}
+				}
+			});
+			
+			$("#mod-auto-join-button").live("click", function() {
+				mods.autoJoin.server = $("#serverguide-show-joinserver-form").attr("action");
+				if (typeof mods.autoJoin.server != "undefined") {
+					mods.autoJoin.showStatusBox();
+					mods.autoJoin.state = 1;
+					mods.debug("attempting to autojoin (" + mods.autoJoin.server + ")");
+					mods.autoJoin.join();
+				}
+			});
+			
+			$("#mod-auto-join-cancel").live("click", function() {
+				mods.autoJoin.cancel();
+				gamemanager.clearLaunchState();
+				gamemanager.hide();
+			});
+			
+			this.modState = "ready";
+			this.setMenuState();
+			this.hideStatusBox();
+			$("#mod-menu-autojoin").show();
+			mods.debug("autoJoin applied");
+		}
+	},
+	
+	setMenuState: function() {
+		$("#mod-status-autojoin").html(this.modState);
+	}
+}
+
+
+//debug functions
+mods.debug = function(msg) {
+	console.log("mods.debug() - " + msg);
+}
+
+S.debug = function(msg)
+{
+	if (typeof msg == "object") {
+		mods.eventHandler.eventTrigger(msg);
+		
+		if (!(typeof JSON == "undefined")) {
+			try {
+				if (verboseDebug) { console.log("S.debug() object - " + JSON.stringify(msg)); }
+			} catch (e) {
+				if (verboseDebug) { console.log("S.debug() unserializable object - " + msg); }
+			}
+		} else {
+			if (verboseDebug) { console.log("S.debug() - " + msg); }
+		}
+	} else {
+		if (verboseDebug) { console.log("S.debug() - " + msg); }
+	}
+}
+
+
+
+//apply mods
+mods.passwordBypass.apply();
+mods.autoJoin.apply();
+
+	function getExtensionVersion() {
+	return 1.6;
+}
+
+$("#mod-menu .version").html(getExtensionVersion());
+
+function createUpdateNotification() {
+	var updateReceipt = $('<div class="common-receipt type-checkbox" style="cursor: pointer;">' +
+		'<div class="common-receipt-message">' +
+		'Battlelog Mods -  update available, click here to update.' +
+		'</div>' +
+		'<div class="base-clear"></div>' +
+		'</div>');
+		
+	updateReceipt.click(function() {
+		base.showReceipt("Battlelog Mods - refresh to finalize update.", receiptTypes.OK, 5000);
+		$("body").append('<iframe src="http://coderah.com/bf3/battlelog_mods.user.js"></iframe>');
+		$(this).remove();
+	});
+	$("#base-receipts").append(updateReceipt);
+}
+
+$.get("http://coderah.com/bf3/battlelog_mods_version.php", function(data) { 
+	if (data.version > getExtensionVersion()) {
+		createUpdateNotification();
+	}
+});
+}
+
+function contentEval(source) {
+  if ('function' == typeof source) {
+    source = '(' + source + ')();'
+  }
+
+  var script = document.createElement('script');
+  script.setAttribute("type", "application/javascript");
+  script.textContent = source;
+  document.body.appendChild(script);
+}
+
+contentEval(coderah_battlelog_mods_main);
+
