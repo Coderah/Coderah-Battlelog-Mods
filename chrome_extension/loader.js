6cd6121cc4b913cfd49a8ef720411cd8a2749f5c
Alex Howard <alex@howardsupply.com>
2011-10-04 01:18:05 -0400

    removed json2.js include from chrome_extension

--- chrome_extension/loader.js
+++ chrome_extension/loader.js
@@ -1,11 +1,6 @@
 var body = document.body;
 var script = document.createElement('script');
 script.type = 'text/javascript';
-script.src = chrome.extension.getURL('json2.js');
-body.appendChild(script);
-
-script = document.createElement('script');
-script.type = 'text/javascript';
 script.src = chrome.extension.getURL('coderah_battlelog_mods.js');
 body.appendChild(script);
 
