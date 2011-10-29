c5497bd76c0f811e73e9808fc7f4ce08b49422a2
Alex Howard <alex@howardsupply.com>
2011-10-28 20:25:49 -0400

    added mods.serverInfo.getDetailed for gathering server details based on a show link

--- code_base/mods.serverMethods.js
+++ code_base/mods.serverMethods.js
@@ -17,5 +17,13 @@ mods.selectedServer = {
 mods.serverInfo = {
 	getStatus: function(url, callback) { //requires playerCountUrl
 		$.get(url, callback);
-	} // callback gets object (first parameter) EX: {"mapMode":64,"players":47,"queued":0,"map":"MP_013"}
+	}, // callback gets object (first parameter) EX: {"mapMode":64,"players":47,"queued":0,"map":"MP_013"}
+	
+	getDetailed: function(url, callback) {
+		$.ajax({
+		  url: url,
+		  success: function(data) { callback(data.context.server); },
+		  beforeSend: function(xhr) { xhr.setRequestHeader("X-AjaxNavigation", "1"); }
+		});
+	} //callback gets detailed server object, url is a valid "/bf3/servers/show/" link
 };
