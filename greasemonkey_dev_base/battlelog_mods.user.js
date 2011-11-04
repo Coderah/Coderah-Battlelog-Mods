0758468060a8f937ac69b3c52bbba28a2752650c
Alex Howard <alex@howardsupply.com>
2011-11-04 08:06:40 -0400

    added greasemonkey_dev_base

---
+++ greasemonkey_dev_base/battlelog_mods.user.js
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name            Coderah Battlelog Mods
+// @author          Alex Howard
+// @namespace       http://www.coderah.com/?page_id=389
+// @description     Multiple modifications for Battlelog (BF3)
+// @version	        <?version?>
+// @include         http://battlelog.battlefield.com/*
+// ==/UserScript==
+
+function coderah_battlelog_mods_main () {
+	<?mainCode?>
+}
+
+function contentEval(source) {
+  if ('function' == typeof source) {
+    source = '(' + source + ')();'
+  }
+
+  var script = document.createElement('script');
+  script.setAttribute("type", "application/javascript");
+  script.textContent = source;
+  document.body.appendChild(script);
+}
+
+contentEval(coderah_battlelog_mods_main);
+
