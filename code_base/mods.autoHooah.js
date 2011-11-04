8eab7737a4ac36da9f7a8bf6091e85dc1daff4a0
Alex Howard <alex@howardsupply.com>
2011-11-04 11:41:44 -0400

    fixed autoHooah link positioning when there is a form (for commenting) in a feed

--- code_base/mods.autoHooah.js
+++ code_base/mods.autoHooah.js
@@ -7,11 +7,34 @@ mods.autoHooah = {
 				type: "addAfter",
 				modify: ['<div id="feed-container">\\n\');', '<div id=\\"feed-container\\">\\n");'],
 				code: function() {
+					mods.debug(o);
+					if (o.feedContext !== "platoon" && o.feedContext !== "profile") {
+						c.push('<span class="mod-auto-hooah">hooah all items</span><span class="mod-auto-hooah undo" style="display:none;">un-hooah all items</span>\n');
+					}
+				}
+			}]);
+			
+			mods.modifyFunction("platoon.index._use_127_9_block_content.render", platoon.index._use_127_9_block_content.render, [{
+				type: "addAfter",
+				modify: ['c.push(\' <div id="platoon-feed">\\n\');', 'c.push(" <div id=\\"platoon-feed\\">\\n");'],
+				code: function() {
 					c.push('<span class="mod-auto-hooah">hooah all items</span><span class="mod-auto-hooah undo" style="display:none;">un-hooah all items</span>\n');
 				}
 			}]);
 			
-			$("#feed-container:visible").prepend($('<span class="mod-auto-hooah">hooah all items</span><span class="mod-auto-hooah undo" style="display:none;">un-hooah all items</span>'));
+			mods.modifyFunction("profile.overview._use_93_22_block_content.render", profile.overview._use_93_22_block_content.render, [{
+				type: "addBefore",
+				modify: ['c.push(\' <form method="POST" class="wallpost" action="\');', 'c.push(" <form method=\\"POST\\" class=\\"wallpost\\" action=\\"");'],
+				code: function() {
+					c.push('<span class="mod-auto-hooah">hooah all items</span><span class="mod-auto-hooah undo" style="display:none;">un-hooah all items</span>\n');
+				}
+			}]);
+			
+			if ($("#platoon-feed, #profile-battlelog .common-box-inner")[0]) {
+				$("#platoon-feed, #profile-battlelog .common-box-inner").prepend($('<span class="mod-auto-hooah">hooah all items</span><span class="mod-auto-hooah undo" style="display:none;">un-hooah all items</span>'));
+			} else {
+				$("#feed-container:visible").prepend($('<span class="mod-auto-hooah">hooah all items</span><span class="mod-auto-hooah undo" style="display:none;">un-hooah all items</span>'));
+			}
 			
 			$(".mod-auto-hooah:not(.undo)").live("click", function() {
 				$("form.feed-like-item:visible a").click();
