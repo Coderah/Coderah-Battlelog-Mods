74b65639f6a906918bd3aca0251946dbf162510f
Alex Howard <alex@howardsupply.com>
2011-11-11 16:19:47 -0500

    added double check on render for autoHideOfflineFriends

--- code_base/mods.autoHideOfflineFriends.js
+++ code_base/mods.autoHideOfflineFriends.js
@@ -1,7 +1,25 @@
 mods.autoHideOfflineFriends = {
+	shouldBeHidden: false,
+	
 	apply:function() {
 		if (mods.settings.autoHideOfflineFriends) {
-			$("#comcenter-offline-separator .dropdownicon").click(); //hide offline friends
+			$("#comcenter-offline-separator.showing-offline .dropdownicon").click(); //hide offline friends
+			this.shouldBeHidden = true;
+			
+			mods.modifyFunction("comcenter.list.surface_95_22.render", comcenter.list.surface_95_22.render, [
+			{
+				type: "addBefore",
+				modify: "var c",
+				code: function() {
+					setTimeout(function() {
+						if (mods.autoHideOfflineFriends.shouldBeHidden) {
+							$("#comcenter-offline-separator.showing-offline .dropdownicon").click();
+							mods.autoHideOfflineFriends.shouldBeHidden = false;
+						}
+					}, 100);
+				}
+			}
+			]);
 			
 			mods.debug("hid offline friends based on setting");
 		}
